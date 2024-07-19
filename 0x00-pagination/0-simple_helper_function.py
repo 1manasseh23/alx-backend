8ba5ab4a1899051c013d41ba3c7bbb81e46fec0d
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-07-19 16:44:46 -0700

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ two integer arguments page and page_size"""
 
 def index_range(page, page_size):
     """
-    The function should return a tuple of size two
+    Return a tuple of size two
     containing a start index and an end index
     corresponding to the range of indexes to return
     in a list for those particular pagination parameters
