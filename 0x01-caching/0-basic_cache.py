6c3c433657bf05fd8f78c7e07d8c33aef73d642d
1manasseh23 <cletavivan@gmail.com>
2024-07-24 07:44:31 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+"""This  a class BasicCache that inherits from
+BaseCaching and is a caching system"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+     BasicCache class that inherits from BaseCaching
+     This caching system has no limit and allows you to
+     store key-value pairs without restriction
+     """
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
