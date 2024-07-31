3c3a58f4201c2e5b92d9db532325b5a4fb48a907
1manasseh23 <cletavivan@gmail.com>
2024-07-31 01:33:51 +0000

    Infer appropriate time zone

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""This  a user login system is outside the scope of this project
-To emulate a similar behavior"""
+"""This a get_timezone function and use the 
+babel.timezoneselector decorator"""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
+import pytz
+from pytz import UnknownTimeZoneError
 
 # Instantiate the Babel object
 babel = Babel()
@@ -31,16 +33,47 @@ def create_app():
 
     @app.route('/')
     def index():
-        return render_template('5-index.html')
+        return render_template('7-index.html')
 
     @babel.localeselector
     def get_locale():
-        # Check if locale is provided in URL parameters
+        # 1. Check if locale is provided in URL parameters
         locale = request.args.get('locale')
         if locale in app.config['LANGUAGES']:
             return locale
-        # Fallback to the best match with the supported languages
+
+        # 2. Check if user is logged in and use their locale
+        if g.user and g.user['locale'] in app.config['LANGUAGES']:
+            return g.user['locale']
+
+        # 3. Fallback to the best match with the supported languages
         return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+    @babel.timezoneselector
+    def get_timezone():
+        # 1. Check if timezone is provided in URL parameters
+        timezone = request.args.get('timezone')
+        if timezone:
+            try:
+                pytz.timezone(timezone)
+                return timezone
+            except UnknownTimeZoneError:
+                pass
+
+        # 2. Check if user is logged in and use their timezone
+        if g.user and g.user['timezone']:
+            try:
+                pytz.timezone(g.user['timezone'])
+                return g.user['timezone']
+            except UnknownTimeZoneError:
+                pass
+
+        # 3. Default to UTC
+        return app.config['BABEL_DEFAULT_TIMEZONE']
+
+    @app.before_request
+    def before_request():
+        g.user = get_user()
 
     def get_user():
         user_id = request.args.get('login_as')
@@ -48,10 +81,6 @@ def create_app():
             user_id = int(user_id)
             return users.get(user_id)
         return None
-
-    @app.before_request
-    def before_request():
-        g.user = get_user()
 
     return app
 
