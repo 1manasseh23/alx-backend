e5d9778206d31ee577f91c7dd0e32c84206dec5c
1manasseh23 <cletavivan@gmail.com>
2024-07-31 11:43:54 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,41 +1,26 @@
-#!/usr/bin/env python3
-"""
-This a get_locale function with the babel.localeselector
-decorator. Use request.accept_languages to determine the
-best match with our supported languages
-"""
-
-from flask import Flask, render_template, request
+#!/usr/bin/python3
+from flask import Flask, request, render_template
 from flask_babel import Babel
 
-# Instantiate the Babel object
-babel = Babel()
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr', 'es']
+
+babel = Babel(app)
 
 
-def create_app():
-    """create flask"""
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages."""
+    return request.accept_languages.best_match(
+            app.config['BABEL_SUPPORTED_LOCALES'])
 
-    app = Flask(__name__)
 
-    # Initialize Babel with the Flask app
-    babel.init_app(app)
-
-    @app.route('/')
-    def index():
-        """Return 2-index.html"""
-
-        return render_template('2-index.html')
-
-    @babel.localeselector
-    def get_locale():
-
-        # Get the best match with the supported languages
-        return request.accept_languages.best_match(["en", "fr"])
-
-    return app
+@app.route('/')
+def index():
+    """Render the index page."""
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
-    """Run the app"""
-    app = create_app()
-    app.run(debug=True)
+    app.run()
